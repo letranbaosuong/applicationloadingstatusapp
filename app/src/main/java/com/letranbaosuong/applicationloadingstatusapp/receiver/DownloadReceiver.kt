2744681f7b3e6c8a931ebd1a7f1df77a5a78f741
letranbaosuong <>
2023-06-27 05:33:44 +0700

    modify

---
+++ app/src/main/java/com/letranbaosuong/applicationloadingstatusapp/receiver/DownloadReceiver.kt
@@ -0,0 +1,29 @@
+package com.letranbaosuong.applicationloadingstatusapp.receiver
+
+import android.app.DownloadManager
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.letranbaosuong.applicationloadingstatusapp.ButtonState
+import com.letranbaosuong.applicationloadingstatusapp.R
+import com.letranbaosuong.applicationloadingstatusapp.utilities.sendNotification
+
+class DownloadReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, downloadID)
+        id?.let {
+            Log.d(TAG, "onReceive EXTRA_DOWNLOAD: Finished with Success: ${getDownloadStatus(id)}")
+            btnDownload.setState(ButtonState.Completed)
+            val notification = NotificationBody(
+                currentOptionName,
+                getDownloadStatus(id)
+            )
+            sendNotification(notification)
+        }
+    }
+
+}
